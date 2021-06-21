0d780d58e84b3906da3b3087e5b50e2f3352a734
Ismail Ajiev <iadjiev@gmail.com>
2021-06-21 23:38:38 +0300

    Added "react-scroll"

--- src/components/atoms/NavigationLink/NavigationLink.tsx
+++ src/components/atoms/NavigationLink/NavigationLink.tsx
@@ -1,5 +1,5 @@
 import React, { FC, memo, ReactNode } from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-scroll'
 import classNames from 'classnames'
 
 type TNavigationLinkProps = {
@@ -10,14 +10,19 @@ type TNavigationLinkProps = {
 
 const NavigationLink: FC<TNavigationLinkProps> = memo(
   ({ children, path, className }: TNavigationLinkProps) => {
-    const defaultClassName = 'text-lg font-mono max-w-max'
+    const defaultClassName = 'has-animated-element cursor-pointer text-lg font-mono max-w-max'
     return (
-      <div className="has-animated-element cursor-pointer">
-        <Link className={classNames(defaultClassName, className)} to={path}>
-          {children}
-          <div className="animated-underline" />
-        </Link>
-      </div>
+      <Link
+        activeClass="active"
+        className={classNames(defaultClassName, className)}
+        to={path}
+        offset={-50}
+        spy
+        smooth
+      >
+        {children}
+        <div className="animated-underline" />
+      </Link>
     )
   }
 )
--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -4,18 +4,21 @@ import { NavigationLink, ThemeTumbler } from '@components/atoms'
 
 type THeaderProps = {}
 
-const Header: FC<THeaderProps> = memo(() => (
-  <>
-    <header className="fixed flex justify-end items-end min-w-full h-24 px-10 pb-4 bg-gradient-to-b from-white dark:from-black">
-      <Navigation>
-        <NavigationLink path="#projects">Hello world!</NavigationLink>
-        <NavigationLink path="">Hello world!!</NavigationLink>
-        <ThemeTumbler className="ml-4" />
-      </Navigation>
-    </header>
-    <div className="mb-24" />
-  </>
-))
+const Header: FC<THeaderProps> = memo(() => {
+
+  return (
+    <>
+      <header className="fixed flex justify-end items-end min-w-full h-24 px-10 pb-4 bg-gradient-to-b from-white dark:from-black">
+        <Navigation>
+          <NavigationLink path="projects">Projects</NavigationLink>
+          <NavigationLink path="">Hello world!!</NavigationLink>
+          <ThemeTumbler className="ml-4" />
+        </Navigation>
+      </header>
+      <div className="mb-24" />
+    </>
+  )
+})
 
 Header.displayName = 'Header'
 
